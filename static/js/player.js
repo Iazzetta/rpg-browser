ec51778301a5f396391122a14eb0a3e1708dd20c
Guilherme Iazzetta <guilherme.iazzetta@tblmanager.com>
2025-05-07 09:33:50 -0300

    sprite finally fixed

--- static/js/player.js
+++ static/js/player.js
@@ -12,19 +12,42 @@ class Player {
         this.delayAttack = delayAttack;
         this.isAttacking = false;
         this.x = 300;
-        this.y = 225;
-        this.width = 200;
-        this.height = 200;
+        this.y = 190;
+        this.width = 162;
+        this.height = 120;
         this.step = speed;
         
         this.keyboard = {left: false, right: false, jump: false}
-
+        
         this.isJumping = false;
         this.jumpMaxHeight = 500;
         this.gravityForce = 15
         this.jumpFall = false;
-
         this.element = document.querySelector('.player')
+        
+        
+        this.offsetX = 0;
+        this.slowedBy = 0;
+        this.slowFrameRate = 4;
+        this.spriteManager = {
+            idle: {
+                src: '/static/img/sprites/warrior/idle.png',
+                frame: 1,
+                frames: 10,
+                width: 162,
+            },
+            attack1: {
+                src: '/static/img/sprites/warrior/attack2.png',
+                frame: 1,
+                frames: 7,
+                width: 162,
+            },
+        }
+        this.selectedSprite = 'idle';
+
+        this.element.style.width = `${this.width}px`;
+        this.element.style.height = `${this.height}px`;
+
 
         this.initEvents();
     }
@@ -52,10 +75,10 @@ class Player {
 
     checkRotation() {
         if (this.x > (window.enemy.x + window.enemy.width)) {
-            this.element.style.transform = 'scaleX(1)';
+            // this.element.style.transform = 'scaleX(1)';
         }
         else if (this.x + this.width < window.enemy.x) {
-            this.element.style.transform = 'scaleX(-1)';
+            // this.element.style.transform = 'scaleX(-1)';
         }
     }
 
@@ -83,10 +106,12 @@ class Player {
 
     attack(entity) {
         if (this.isAttacking) return;
+        this.selectedSprite = 'attack1';
         this.isAttacking = true;
         entity.hp -= this.damage;
         setTimeout(() => {
             this.isAttacking = false;
+            this.selectedSprite = 'idle';
         }, this.delayAttack * 1000);
     }
 
@@ -106,6 +131,27 @@ class Player {
         // player position
         document.querySelector('.player').style.left = `${this.x}px`;
         document.querySelector('.player').style.bottom = `${this.y}px`;
+
+        // draw sprite
+        this.drawSprite()
+    }
+
+    drawSprite() {
+        const selectedFrame = this.spriteManager[this.selectedSprite];
+
+        if (this.slowedBy >= this.slowFrameRate) {
+            if (selectedFrame.frame >= selectedFrame.frames) {
+                selectedFrame.frame = 1;
+            } else {
+                selectedFrame.frame++;
+            }
+            this.slowedBy = 0;
+        } else {
+            this.slowedBy++;
+        }
+        const frameX = selectedFrame.frame * selectedFrame.width;
+        this.element.style.backgroundImage = `url(${selectedFrame.src})`
+        this.element.style.backgroundPosition = `${frameX}px ${this.height}px`
     }
 
     checkLevelUp() {
